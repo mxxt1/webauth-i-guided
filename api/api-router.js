28973a87e0c1fec88c2d15a7d76257c819b651e2
mxxt1 <matt@dustcloudlabs.com>
2019-11-11 12:30:59 -0500

    added pw hashing api-router

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -8,6 +8,24 @@ router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post('/hash', (req, res) => {
+  //read pass from body
+  const  pw = req.body.password;
+
+  //hash pw
+
+  req.body.password = bcrypt.hashSync(pw, 12);
+
+  //return to user in an object
+
+  res.status(200).json({password: `${pw}`, hashed: `${req.body.password}`});
+
+
+
+
+
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const apiRouter = require('./api-router.js');
 
+const configureMiddleWare = require('./configure-middleware.js');
+
 const server = express();
+
+configureMiddleWare(server);
 
 server.use('/api', apiRouter);
 
